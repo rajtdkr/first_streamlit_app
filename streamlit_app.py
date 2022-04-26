4750f2f1d07ff088722f44faf818bbbf4260b281
Raj Tandukar <rajtdkr@gmail.com>
2022-04-26 14:54:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlist.text(fruityvice_response)
+streamlit.text(fruityvice_response)
